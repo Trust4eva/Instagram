9f7d5235dacf5953895ab397695973d269659827
Trustin Harris <trust@Trustins-MacBook-Pro.local>
2018-02-21 18:50:36 -0500

    commit

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,8 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+        
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "Instagram"
+                configuration.clientKey = "fbjadfnfd"
+                configuration.server = "https://floating-spire-49509.herokuapp.com/parse"
+            })
+        )
+                return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- Instagram/LoginViewController.swift
+++ Instagram/LoginViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,15 +26,34 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func onSignIn(_ sender: Any) {
+        
+        PFUser.logInWithUsername(inBackground: usernameField.text! , password: passwordField.text!) { (user: PFUser? , error: Error?) in
+            if user != nil {
+                print("You're logged in")
+                self.performSegue(withIdentifier: "LoginSegue", sender: nil)
+            }
+        }
+        
     }
-    */
+    
 
+    @IBAction func onSignUp(_ sender: Any) {
+        let newUser = PFUser()
+        
+        newUser.username = usernameField.text
+        
+        newUser.password = passwordField.text
+        newUser.signUpInBackground { (success: Bool, error: Error) in
+            if success {
+                print("Yay")
+            } else {
+                print(error?.localizedDescription)
+                if error?.code == 202 {
+                    print("Username is taken")
+                }
+            }
+        }
+    }
+    
 }
